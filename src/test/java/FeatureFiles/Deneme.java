3cb32165d30de8c2ee0df48a2c9c43a48e0d0941
mustafa Likos <likosmustafa@gmail.com>
2023-04-12 21:25:51 +0300

    Deneme iki basarılı

--- src/test/java/FeatureFiles/Deneme.java
+++ src/test/java/FeatureFiles/Deneme.java
@@ -3,5 +3,6 @@ package FeatureFiles;
 public class Deneme {
     public static void main(String[] args) {
         System.out.println("Deneme Mustafa");
+        System.out.println("Deneme iki");
     }
 }
